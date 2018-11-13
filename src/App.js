4e1bcd0c23a3a5f0d01b68443e095321b945b449
Riccardo Coppola <Riccardo.Coppola@thetrainline.com>
2018-11-13 23:41:32 +0000

    Stage 1: Main component, post component into main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="blog">
+        <header className="post-header">
+          <h1>A new blog post</h1>
         </header>
+        <section className="post-body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque eu tincidunt tortor aliquam. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Vitae semper quis lectus nulla at volutpat diam ut venenatis
+        </section>
       </div>
     );
   }
